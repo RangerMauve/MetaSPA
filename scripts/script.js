a84fc4ab261bd1bbf9d584c221349723b37a76ec
RangerMauve <RangerMauve@hotmail.com>
2014-01-17 08:53:53 -0500

    Added smooth hiding of inactive elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
-$("menu label").click(function(){
+$("menu label").click(function () {
 	$(this).parent().toggleClass("active");
 });
 
-$("nav > label").click(function(){
+$("nav > label").click(function () {
 	$("nav > label, main > section").removeClass("active");
+	$("#" + this.dataset.tabId).addClass("active");
 	$(this).addClass("active");
-	$("#"+this.dataset.tabId).addClass("active");
 	$(".tabInfo").text("Tab: " + this.dataset.tabId);
 })
